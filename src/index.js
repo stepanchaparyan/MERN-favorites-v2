ed47bcd606278e83d73f01e1b83d6aa9ecc86c07
stepanchaparyan <stepanchaparyan@gmail.com>
2021-05-06 23:10:11 +0400

    added normalize import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import 'normalize.css';
 import './index.css';
 import App from './App';
 import store from './redux/store/store';
