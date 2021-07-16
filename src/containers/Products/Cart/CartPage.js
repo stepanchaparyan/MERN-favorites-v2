a75edd1da599462c3a542f5db4bb94e9c569ba50
stepanchaparyan <stepanchaparyan@gmail.com>
2021-07-16 23:54:53 +0400

    added loadind on carts page

--- src/containers/Products/Cart/CartPage.js
+++ src/containers/Products/Cart/CartPage.js
@@ -14,13 +14,14 @@ import {
   ButtonContainer,
   ButtonStyled
 } from './CartPageStyled';
+import Loading from '../../../components/Loading/Loading';
 
 const CartPage = () => {
   const dispatch = useDispatch();
   const { pathname } = useLocation();
 
   const shopingCart = useSelector(state => state.shopingCart);
-  const { cartItems } = shopingCart;
+  const { cartItems, loading, error } = shopingCart;
 
   useEffect(() => {
     dispatch(getMyCartItems());
@@ -41,33 +42,41 @@ const CartPage = () => {
 
   return (
     <Container>
-      <PageTitle>Shopping Cart</PageTitle>
-      <>
-        <CartsContainer>
-          {!cartItems?.length ? (
-            <EmptyCart>
-              Your Cart Is Empty <Link to={LINK.TO.HOME}>Go Back</Link>
-            </EmptyCart>
-          ) : (
-            cartItems?.map(item => (
-              <CartItem
-                key={item._id}
-                item={item}
-                removeHandler={() => removeFromCartHandler(item._id)}
-              />
-            ))
-          )}
-        </CartsContainer>
-        <Checkout>
-          <Info>
-            <p>Subtotal ({getCartCount()}) items</p>
-            <p>${getCartSubTotal()}</p>
-          </Info>
-          <ButtonContainer>
-            <ButtonStyled>Proceed To Checkout</ButtonStyled>
-          </ButtonContainer>
-        </Checkout>
-      </>
+      {loading ? (
+        <Loading type="ThreeDots" color="cadetblue" height={80} width={80}></Loading>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <>
+          <PageTitle>Shopping Cart</PageTitle>
+          <>
+            <CartsContainer>
+              {!cartItems?.length ? (
+                <EmptyCart>
+                  Your Cart Is Empty <Link to={LINK.TO.HOME}>Go Back</Link>
+                </EmptyCart>
+              ) : (
+                cartItems?.map(item => (
+                  <CartItem
+                    key={item._id}
+                    item={item}
+                    removeHandler={() => removeFromCartHandler(item._id)}
+                  />
+                ))
+              )}
+            </CartsContainer>
+            <Checkout>
+              <Info>
+                <p>Subtotal ({getCartCount()}) items</p>
+                <p>${getCartSubTotal()}</p>
+              </Info>
+              <ButtonContainer>
+                <ButtonStyled>Proceed To Checkout</ButtonStyled>
+              </ButtonContainer>
+            </Checkout>
+          </>
+        </>
+      )}
     </Container>
   );
 };
