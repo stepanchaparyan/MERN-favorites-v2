6cca19d1628885ea6132e72a60fad9c8e2baf10b
stepanchaparyan <stepanchaparyan@gmail.com>
2021-09-07 01:45:49 +0400

    Update auth-token inti Authentication Beare token

--- src/constants/headers.js
+++ src/constants/headers.js
@@ -1,7 +1,8 @@
 export const HEADER_CONFIG = {
   CONTENT_TYPE_APPLICATION_JSON: {
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      credentials: true
     }
   },
   CONTENT_TYPE_MULTIPART_FORM_DATA: { 'Content-Type': 'multipart/form-data' }
--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const auth_token = 'auth-token';
+const Authorization = 'Authorization';
 
 const setAuthToken = token => {
   if (token) {
-    axios.defaults.headers.common[auth_token] = token;
+    axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
   } else {
-    delete axios.defaults.headers.common[auth_token];
+    delete axios.defaults.headers.common[Authorization];
   }
 };
 export default setAuthToken;
