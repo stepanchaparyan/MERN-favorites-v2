29cc27ad34ed2099ef48bb75038069729123eadf
Stepan Chaparyan <stepan.chaparyan@dataart.com>
2022-06-21 11:08:55 +0400

    update router from hashrouter and helmet for Profile page

--- src/containers/pages/Profile/Profile.js
+++ src/containers/pages/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useIntl } from 'react-intl';
+import Helmet from 'react-helmet';
 import ProfileContext from '../../../context/profileContext/profileContext';
 import ProfileForm from '../ProfileForm/ProfileForm';
 import FileUpload from '../ProfileForm/FileUpload';
@@ -62,57 +63,60 @@ const Profile = () => {
   };
 
   return (
-    <Container>
-      <Module>
-        {message ? <Message msg={message} /> : null}
-        <WelcomeText>{formatMessage(localization.profilePage)}</WelcomeText>
-        {!toggleForm && (
-          <>
-            <Info>
-              <ImageContainer>
-                <ProfileImage src={require(`../../../assets/${profile.image}`)} />
-              </ImageContainer>
-              <InfoData>
-                <ProfileData>
-                  <Text>{formatMessage(localization.name)}:</Text>
-                  <Data>{profile.name}</Data>
-                </ProfileData>
-                <ProfileData>
-                  <Text>{formatMessage(localization.surname)}:</Text>
-                  <Data>{profile.surname}</Data>
-                </ProfileData>
-                <ProfileData>
-                  <Text>{formatMessage(localization.email)}:</Text>
-                  <Data data-tip={profile.email} data-arrow-color={cadetblue} data-background-color={cadetblue}>
-                    {profile.email}
-                  </Data>
-                </ProfileData>
-                <ProfileData>
-                  <Text>{formatMessage(localization.gender)}:</Text>
-                  <Data>{profile.gender}</Data>
-                </ProfileData>
-                <ProfileData>
-                  <Text>{formatMessage(localization.birthday)}:</Text>
-                  <Data>{moment(profile.birthDay).format('ll')}</Data>
-                </ProfileData>
-                <ProfileData>
-                  <Text>{formatMessage(localization.phone)}:</Text>
-                  <Data>{profile.phoneNumber}</Data>
-                </ProfileData>
-              </InfoData>
-            </Info>
-            <ReactTooltipStyled place={PLACE.RIGHT} effect={EFFECT.SOLID} />
-            <Button onClick={handleEdit}>{formatMessage(localization.editCard)}</Button>
-          </>
-        )}
-        <Forms>
-          {toggleForm && <FileUpload />}
-          {toggleForm && <ProfileForm />}
-        </Forms>
-        <LinkStyled to={LINK.TO.WELCOME}>{formatMessage(localization.goHomePage)}</LinkStyled>
-        <Logo src={Img}></Logo>
-      </Module>
-    </Container>
+    <>
+      <Helmet title='Profile Page'></Helmet>
+      <Container>
+        <Module>
+          {message ? <Message msg={message} /> : null}
+          <WelcomeText>{formatMessage(localization.profilePage)}</WelcomeText>
+          {!toggleForm && (
+            <>
+              <Info>
+                <ImageContainer>
+                  <ProfileImage src={require(`../../../assets/${profile.image}`)} />
+                </ImageContainer>
+                <InfoData>
+                  <ProfileData>
+                    <Text>{formatMessage(localization.name)}:</Text>
+                    <Data>{profile.name}</Data>
+                  </ProfileData>
+                  <ProfileData>
+                    <Text>{formatMessage(localization.surname)}:</Text>
+                    <Data>{profile.surname}</Data>
+                  </ProfileData>
+                  <ProfileData>
+                    <Text>{formatMessage(localization.email)}:</Text>
+                    <Data data-tip={profile.email} data-arrow-color={cadetblue} data-background-color={cadetblue}>
+                      {profile.email}
+                    </Data>
+                  </ProfileData>
+                  <ProfileData>
+                    <Text>{formatMessage(localization.gender)}:</Text>
+                    <Data>{profile.gender}</Data>
+                  </ProfileData>
+                  <ProfileData>
+                    <Text>{formatMessage(localization.birthday)}:</Text>
+                    <Data>{moment(profile.birthDay).format('ll')}</Data>
+                  </ProfileData>
+                  <ProfileData>
+                    <Text>{formatMessage(localization.phone)}:</Text>
+                    <Data>{profile.phoneNumber}</Data>
+                  </ProfileData>
+                </InfoData>
+              </Info>
+              <ReactTooltipStyled place={PLACE.RIGHT} effect={EFFECT.SOLID} />
+              <Button onClick={handleEdit}>{formatMessage(localization.editCard)}</Button>
+            </>
+          )}
+          <Forms>
+            {toggleForm && <FileUpload />}
+            {toggleForm && <ProfileForm />}
+          </Forms>
+          <LinkStyled to={LINK.TO.WELCOME}>{formatMessage(localization.goHomePage)}</LinkStyled>
+          <Logo src={Img}></Logo>
+        </Module>
+      </Container>
+    </>
   );
 };
 export default Profile;
